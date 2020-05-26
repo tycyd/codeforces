63725dfa1032cde4b8c014374f9615407eb6b8fb
tycyd <tycyd@hotmail.com>
2020-05-25 17:13:36 -0700

    Create 1355D Game With Array.py

---
+++ 1355D Game With Array.py
@@ -0,0 +1,34 @@
+
+# N: length of array
+# S: required sum
+# K: K or S-K , 2 10-2=8
+# 1=>{4}, 2=>{1,3} {2,2} {3,1} 3=>{1,1,2} {1,2,1} {2,1,1}
+# 6=>{1,1,4}   min:1   max:4  => 1 1 4
+# 6=>{1 1 1 3} min:1   max:3
+# 6=>{1 3 1 1} min:1   max:3
+# 6=>{2 1 1 2} min:1   max:3
+# 6=>{2 2 2}
+# 8=>{1 1 6} 1~4
+# 6=>{1 1 4} 1~3
+# 1=>{1}
+# 9=>{1 1 1 1 1 1 3}  (1-4)
+
+def game_with_arry(N, S):
+
+    ary = [1 for i in range(N)]
+    ary[0] = S - N + 1
+
+    if S//2 > N - 1:
+        print("YES")
+        print(*ary)
+        print(N)
+    else:
+        print("NO")
+
+
+if __name__ == '__main__':
+    NS = input().split(' ')
+    N = int(NS[0])
+    S = int(NS[1])
+
+    game_with_arry(N, S)
