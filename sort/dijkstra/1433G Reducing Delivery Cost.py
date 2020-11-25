0fb103c2b03093386d3f1ba923042a5753a46c56
yaodi <tycyd@hotmail.com>
2020-11-25 11:02:11 -0800

    macbook pro

--- sort/dijkstra/1433G Reducing Delivery Cost.py
+++ sort/dijkstra/1433G Reducing Delivery Cost.py
@@ -2,6 +2,55 @@ from sys import stdin, stdout
 from heapq import *
 
 
+def pre_calc_shortest_path(a, dic_r, c_a):
+    h_a = [[0, a]]
+    c_a[a][a] = 0
+
+    while h_a:
+        cur = h_a.pop()
+
+        for p, w in dic_r[cur[1]]:
+            if c_a[a][p] != -1 and c_a[a][p] <= cur[0] + w:
+                continue
+
+            c_a[a][p] = cur[0] + w
+            h_a.append([cur[0] + w, p])
+
+    return
+
+
+def pre_calc_shortest_path2(a, dic_r, c_a):
+    s = set()
+    h_a = [[0, a]]
+
+    while h_a:
+        cur = heappop(h_a)
+        c_a[a][cur[1]] = cur[0]
+
+        s.add(cur[1])
+
+        for p, w in dic_r[cur[1]]:
+            if p in s:
+                continue
+            heappush(h_a, [w + cur[0], p])
+
+    return
+
+
+def reducing_delivery_cost(m_a, c_a, k_a, ores):
+    res = ores
+
+    for m in m_a:
+        cur = ores
+        for k in k_a:
+            r = min(c_a[k[0]][m[0]] + c_a[k[1]][m[1]], c_a[k[0]][m[1]] + c_a[k[1]][m[0]])
+            if c_a[k[0]][k[1]] > r:
+                cur -= c_a[k[0]][k[1]] - r
+        res = min(res, cur)
+
+    return res
+
+
 def calc_shortest_path(a, b, dic_r):
     s = set()
     h_a = [[0, a]]
@@ -21,7 +70,7 @@ def calc_shortest_path(a, b, dic_r):
     return -1
 
 
-def reducing_delivery_cost(m_a, c_a, dic_r, k_a, ores):
+def reducing_delivery_cost2(m_a, c_a, dic_r, k_a, ores):
 
     res = ores
 
@@ -84,16 +133,22 @@ for _ in range(m):
     dic_r[y].append([x, w])
     m_a.append([x, y])
 
+#for i in range(1, n+1):
+#    dic_r[i].sort(key=lambda x: x[1])
+
+for i in range(1, n+1):
+    pre_calc_shortest_path(i, dic_r, c_a)
+
 ores = 0
 k_a = []
 for _ in range(k):
     a, b = map(int,  stdin.readline().split())
     k_a.append([a, b])
 
-    if c_a[a][b] == -1:
-        c_a[a][b] = calc_shortest_path(a, b, dic_r)
-        c_a[b][a] = c_a[a][b]
+    #if c_a[a][b] == -1:
+    #    c_a[a][b] = calc_shortest_path(a, b, dic_r)
+    #    c_a[b][a] = c_a[a][b]
     ores += c_a[a][b]
 
-res = reducing_delivery_cost(m_a, c_a, dic_r, k_a, ores)
+res = reducing_delivery_cost(m_a, c_a, k_a, ores)
 stdout.write(str(res))
